a5c4a02bb0249d1ee5e4df1be7110cbb12a923b6
RedRNS <putrirenisa@gmail.com>
2025-01-06 17:06:59 +0700

    Initial commit

---
+++ coba.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, total, pembagi int64
+	total, pembagi = 0, 0
+	fmt.Scan(&n)
+
+	for {
+		if n == 9999 {
+			break
+		}
+		pembagi++
+		total += n
+		fmt.Scan(&n)
+	}
+	fmt.Print(total / pembagi)
+}
